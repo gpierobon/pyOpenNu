78ec87f98b8354031cf1c17f9c8c0e3b507b8b4e
gpierobon <g.pierobon@unsw.edu.au>
2025-08-01 17:20:12 +1000

    saving local work

--- gammas.py
+++ gammas.py
@@ -2,44 +2,39 @@ import numpy as np
 from scipy.integrate import dblquad
 from mpmath import mp, quad, sqrt, exp, inf, fabs, findroot
 
-A = 1
-Z = 1
-ga = np.array([-A/2 + Z, -A/2 + Z, -A/2 + Z])
 
-A = 129
-Z = 54
-ga_Xe = np.array([-A/2 + Z, -A/2 + Z, -A/2 + Z])
 
-# PMNS Matrix angles and CP phase
-theta12 = np.deg2rad(33.82)
-theta13 = np.deg2rad(8.62)
-theta23 = np.deg2rad(45.0)
-delta   = np.deg2rad(197)
+def compute_gav_matrix(A, Z):
+    '''
+    '''
+    ga = np.array([-A / 2 + Z, -A / 2 + Z, -A / 2 + Z])
+    
+    # PMNS matrix parameters
+    theta12 = np.deg2rad(33.82)
+    theta13 = np.deg2rad(8.62)
+    theta23 = np.deg2rad(45.0)
+    delta = np.deg2rad(197)
+    
+    # PMNS matrix
+    U = np.array([
+        [np.cos(theta12) * np.cos(theta13), np.sin(theta12) * np.cos(theta13), np.sin(theta13) * np.exp(-1j * delta)],
+        [-np.sin(theta12) * np.cos(theta23) - np.cos(theta12) * np.sin(theta23) * np.sin(theta13) * np.exp(1j * delta),
+         np.cos(theta12) * np.cos(theta23) - np.sin(theta12) * np.sin(theta23) * np.sin(theta13) * np.exp(1j * delta),
+         np.sin(theta23) * np.cos(theta13)],
+        [np.sin(theta12) * np.sin(theta23) - np.cos(theta12) * np.cos(theta23) * np.sin(theta13) * np.exp(1j * delta),
+         -np.cos(theta12) * np.sin(theta23) - np.sin(theta12) * np.cos(theta23) * np.sin(theta13) * np.exp(1j * delta),
+         np.cos(theta23) * np.cos(theta13)]
+    ])
 
-# PMNS matrix
-U = np.array([
-    [np.cos(theta12)*np.cos(theta13), np.sin(theta12)*np.cos(theta13), np.sin(theta13)*np.exp(-1j*delta)],
-    [-np.sin(theta12)*np.cos(theta23) - np.cos(theta12)*np.sin(theta23)*np.sin(theta13)*np.exp(1j*delta),
-     np.cos(theta12)*np.cos(theta23) - np.sin(theta12)*np.sin(theta23)*np.sin(theta13)*np.exp(1j*delta),
-     np.sin(theta23)*np.cos(theta13)],
-    [np.sin(theta12)*np.sin(theta23) - np.cos(theta12)*np.cos(theta23)*np.sin(theta13)*np.exp(1j*delta),
-     -np.cos(theta12)*np.sin(theta23) - np.sin(theta12)*np.cos(theta23)*np.sin(theta13)*np.exp(1j*delta),
-     np.cos(theta23)*np.cos(theta13)]
-])
-
-gav = np.zeros((3, 3), dtype=complex)
-for i in range(3):
-    for j in range(3):
-        gav[i, j] = sum(ga[k] * np.conj(U[k, i]) * U[k, j] for k in range(3))
-
-gav_Xe = np.zeros((3, 3), dtype=complex)
-for i in range(3):
-    for j in range(3):
-        gav[i, j] = sum(ga[k] * np.conj(U[k, i]) * U[k, j] for k in range(3))
+    gav = np.zeros((3, 3), dtype=complex)
+    for i in range(3):
+        for j in range(3):
+            gav[i, j] = sum(ga[k] * np.conj(U[k, i]) * U[k, j] for k in range(3))
+    return gav
 
 
 
-def compute_gammas(w_val, m_vals, gav):
+def compute_gammas(w_val, m_vals, A, Z):
     eV_2_Hz = 1/6.58e-16
     mp.dps = 70  # High precision
 
@@ -47,6 +42,8 @@ def compute_gammas(w_val, m_vals, gav):
     Gf = mp.mpf("1.17e-23")   # Fermi constant (example)
     w = mp.mpf(w_val)
     m = [mp.mpf(mval) for mval in m_vals]
+
+    gav = compute_gav_matrix(A, Z)
 
     def integrand_p(p, m1, m2, w):
         E1 = sqrt(p**2 + m1**2)
@@ -83,14 +80,18 @@ def compute_gammas(w_val, m_vals, gav):
             result_m = quad(lambda p: integrand_m(p, m[i], m[j], w), [0, inf])
             inam[i][j] = fabs(gav[i][j] * gav[j][i]) * result_m
 
-    prefactor = (4 * Gf**2) / (2 * mp.pi)**3
+    if majorana:
+        pref = 2
+    else:
+        pref = 1
+    prefactor = pref * (4 * Gf**2) / (2 * mp.pi)**3
     gammap = prefactor * sum(sum(row) for row in ina)
     gammam = prefactor * sum(sum(row) for row in inam)
 
     return float(gammap), float(gammam), float(gammap*eV_2_Hz), float(gammam*eV_2_Hz)
 
 
-def compute_ratio(m, w):
+def compute_ratio(m, w, A=129, Z=54):
     mnu = mp.mpf(m) 
     w0 = mp.mpf(w)
 
@@ -106,39 +107,126 @@ def compute_ratio(m, w):
 
     def get_masses(m_sum):
         # Solve m1 from total mass sum
-        m1 = findroot(lambda m1: mass_sum_difference(m1, m_sum), 0.001)
-        m2 = sqrt(m1**2 + dm21)
-        m3 = sqrt(m1**2 + dm31)
-        return [m1, m2, m3]
+        if m_sum< 0.06:
+            ms = m_sum/3
+            return [ms, ms, ms]
+        else:
+            m1 = findroot(lambda m1: mass_sum_difference(m1, m_sum), 0.001)
+            m2 = sqrt(m1**2 + dm21)
+            m3 = sqrt(m1**2 + dm31)
+    
+            return [m1, m2, m3]
 
     mm = get_masses(mnu)
-    gp_eV, gm_eV, gp, gm = compute_gammas(w0, [mm[0], mm[1], mm[2]], gav)
+    gp_eV, gm_eV, gp, gm = compute_gammas(w0, [mm[0], mm[1], mm[2]], A, Z)
     return gp/gm, gm
 
 
-def oldGammas(m, w0):
-    u  = 0.25     # CKM coefficient
-    de = 3        # 3 Dirac, 6 Majorana
 
-    int_lim = 10
+# # Mass generation function (NH/IH/degenerate)
+# def get_masses(m_sum=None, m0=None, hierarchy="normal", mode="exact"):
+#     dm21 = mp.mpf("7.4e-5")
+#     dm31 = mp.mpf("2.5e-3")
 
-    eV_2_Hz = 1/6.58e-16           # convert eV to Hz
-    K_2_eV = 8.62e-5               # convert Kelvin to eV
+#     if mode == "degenerate":
+#         return [mp.mpf(m0)] * 3
 
-    Tnu  = 1.95*K_2_eV    # eV
-    GF   = 1.17e-23       # eV^-2
+#     if hierarchy == "normal":
+#         if m_sum is not None:
+#             def f(m1): return m1 + sqrt(m1**2 + dm21) + sqrt(m1**2 + dm31) - m_sum
+#             m1 = findroot(f, 0.001)
+#         else:
+#             m1 = mp.mpf(m0)
+#         return [m1, sqrt(m1**2 + dm21), sqrt(m1**2 + dm31)]
 
-    norm = de * eV_2_Hz**2 * (4 * np.pi * GF**2 * m * u) / (np.pi**4 * 8)
+#     elif hierarchy == "inverted":
+#         if m_sum is not None:
+#             def f(m3): return sqrt(m3**2 + dm31) + sqrt(m3**2 + dm31 + dm21) + m3 - m_sum
+#             m3 = findroot(f, 0.001)
+#         else:
+#             m3 = mp.mpf(m0)
+#         m1 = sqrt(m3**2 + dm31)
+#         m2 = sqrt(m3**2 + dm31 + dm21)
+#         return [m1, m2, m3]
 
-    def integrand_m(k, w):
-        return 2 * k**2 * np.sqrt(k**2 - 2 * w * m) / (np.exp(k / Tnu) + 1)
+#     else:
+#         raise ValueError("Hierarchy must be 'normal', 'inverted', or use mode='degenerate'.")
 
-    def integrand_p(k, w):
-        return 2 * k**2 * np.sqrt(k**2 + 2 * w * m) / (np.exp(k / Tnu) + 1)
+# def compute_ratio(w, m_sum=None, m0=None, hierarchy="normal", mode="exact"):
+#     w0 = mp.mpf(w)
+#     mm = get_masses(m_sum=m_sum, m0=m0, hierarchy=hierarchy, mode=mode)
+#     gp_eV, gm_eV, gp_Hz, gm_Hz = compute_gammas(w0, mm, gav)
+#     return gp_eV / gm_eV, gm_eV
 
-    gp, _ = dblquad(lambda x, k: norm*integrand_p(k, w0), np.sqrt((m + w0)**2 - m**2), int_lim, -1, 1)
-    gm, _ = dblquad(lambda x, k: norm*integrand_m(k, w0), 0, int_lim, -1, 1)
 
-    return gm*eV_2_Hz, gp*eV_2_Hz
+
+
+
+
+
+
+
+
+
+# A = 1
+# Z = 1
+# ga = np.array([-A/2 + Z, -A/2 + Z, -A/2 + Z])
+
+# A = 129
+# Z = 54
+# ga_Xe = np.array([-A/2 + Z, -A/2 + Z, -A/2 + Z])
+
+# # PMNS Matrix angles and CP phase
+# theta12 = np.deg2rad(33.82)
+# theta13 = np.deg2rad(8.62)
+# theta23 = np.deg2rad(45.0)
+# delta   = np.deg2rad(197)
+
+# # PMNS matrix
+# U = np.array([
+#     [np.cos(theta12)*np.cos(theta13), np.sin(theta12)*np.cos(theta13), np.sin(theta13)*np.exp(-1j*delta)],
+#     [-np.sin(theta12)*np.cos(theta23) - np.cos(theta12)*np.sin(theta23)*np.sin(theta13)*np.exp(1j*delta),
+#      np.cos(theta12)*np.cos(theta23) - np.sin(theta12)*np.sin(theta23)*np.sin(theta13)*np.exp(1j*delta),
+#      np.sin(theta23)*np.cos(theta13)],
+#     [np.sin(theta12)*np.sin(theta23) - np.cos(theta12)*np.cos(theta23)*np.sin(theta13)*np.exp(1j*delta),
+#      -np.cos(theta12)*np.sin(theta23) - np.sin(theta12)*np.cos(theta23)*np.sin(theta13)*np.exp(1j*delta),
+#      np.cos(theta23)*np.cos(theta13)]
+# ])
+
+# gav = np.zeros((3, 3), dtype=complex)
+# for i in range(3):
+#     for j in range(3):
+#         gav[i, j] = sum(ga[k] * np.conj(U[k, i]) * U[k, j] for k in range(3))
+
+# gav_Xe = np.zeros((3, 3), dtype=complex)
+# for i in range(3):
+#     for j in range(3):
+#         gav_Xe[i, j] = sum(ga_Xe[k] * np.conj(U[k, i]) * U[k, j] for k in range(3))
+
+
+# def oldGammas(m, w0):
+#     u  = 0.25     # CKM coefficient
+#     de = 3        # 3 Dirac, 6 Majorana
+
+#     int_lim = 10
+
+#     eV_2_Hz = 1/6.58e-16           # convert eV to Hz
+#     K_2_eV = 8.62e-5               # convert Kelvin to eV
+
+#     Tnu  = 1.95*K_2_eV    # eV
+#     GF   = 1.17e-23       # eV^-2
+
+#     norm = de * eV_2_Hz**2 * (4 * np.pi * GF**2 * m * u) / (np.pi**4 * 8)
+
+#     def integrand_m(k, w):
+#         return 2 * k**2 * np.sqrt(k**2 - 2 * w * m) / (np.exp(k / Tnu) + 1)
+
+#     def integrand_p(k, w):
+#         return 2 * k**2 * np.sqrt(k**2 + 2 * w * m) / (np.exp(k / Tnu) + 1)
+
+#     gp, _ = dblquad(lambda x, k: norm*integrand_p(k, w0), np.sqrt((m + w0)**2 - m**2), int_lim, -1, 1)
+#     gm, _ = dblquad(lambda x, k: norm*integrand_m(k, w0), 0, int_lim, -1, 1)
+
+#     return gm*eV_2_Hz, gp*eV_2_Hz
 
 
